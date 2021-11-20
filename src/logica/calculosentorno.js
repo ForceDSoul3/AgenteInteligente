f8d682442b369b0121946a72d3addcccc6b3d690
FDS3 <55816760+ForceDSoul3@users.noreply.github.com>
2021-11-20 15:13:36 -0600

    Adjusted code, deleted unnecesary stuff

--- src/logica/calculosentorno.js
+++ src/logica/calculosentorno.js
@@ -1,21 +1,28 @@
 
 //FILL ARRAY THAT REPRESENTS TH ENVIROMENT INCLUDES THE DEFAULT OBSTACLES ON 0X 0Y AND MAX X Y
 export const crearentorno = () => {
+    //BY DEFAULT AND RULES, OUR AGENT IS IN 1,1
     const x = 1
     const y = 1
+
+    //WE INITIALIZE THE ARRAY FOR OUR OBJECTS(400 objects)
     let arr = [];
     var id = 400;
     for(let i=19;i>=0;i--){
+        //Needed to fill ids in the desired order, from right to left, up to down
         id=id-20;
         for(let j=0;j<20;j++){
+            //SETS THE AGENT IN 1,1 by default
             if(j === x && i === y){
                 arr.push({'id':id,'x':j,'y':i,'obs':false,'meta':false,'visitado':false,'isAgente': true});
                 id++;
             }
+            //TO ALL X0 && Y0, X19 && Y19, set to obstacle, to create default walls
             else if(j === 0 || i === 0 || j ===19 || i === 19){
                 arr.push({'id':id,'x':j,'y':i,'obs':true,'meta':false,'visitado':false,'isAgente': false});
                 id++;
             }
+            //FOR EVERY ELSE OBJECT IN THE ARRAY WE SET ALL THE STATES ON FALSE
             else{
                 arr.push({'id':id,'x':j,'y':i,'obs':false,'meta':false,'visitado':false,'isAgente': false});
                 id++;
@@ -30,10 +37,15 @@ export const crearentorno = () => {
 
 //TAKES THE POSITION OF THE AGENT AND PRINTS IT INSIDE THE ENVIROMENT ARRAY
 export const colorearagente = ({agente_p,tablero}) => {
+    //EXTRACT THE X AND Y COORDS
     let ax = agente_p.x
     let ay = agente_p.y
+
+    //WE START WORKING WITH THE OBJECTS ARRAY
     let cleanAgente = []
     cleanAgente = tablero
+    //WE NEED TO WALK OVER ALL THE OBJECTS TO AVOID DUPLICATE REPRESENTATIONS 
+    //OF THE AGENT
     for(let i=0; i<=399; i++){
         if(cleanAgente[i].isAgente === true){
             cleanAgente[i].isAgente = false
@@ -50,13 +62,20 @@ export const colorearagente = ({agente_p,tablero}) => {
 
 //USED TO SET THE PLACE WHERE THE GOAL IS
 export const colorearmeta = ({tablero, meta}) =>{
+    //EXTRACT THE DATA FROM THE INPUT, CONTAINS AN ID
     const casilla = parseInt(meta)
+
+    //WE START WORKING WITH THE ARRAY OF OBJECTS
     let ajuste = []
     ajuste = tablero
+
+    //WE WALK OVER ALL THE ARRAY TO FIND A MATCH IN THE ID
     for(let i=0; i<=399; i++){
         if(ajuste[i].id===casilla){
             ajuste[i].meta = true            
         }
+        //TO DELETE ANY OTHER DEFINITION OF THE "meta"
+        //AVOID DUPLICATED GOALS
         else{
             ajuste[i].meta = false
         }
@@ -69,13 +88,13 @@ export const colorearmeta = ({tablero, meta}) =>{
 
 //INSERTS AND OBSTACLE
 export const colorearobstaculo = ({tablero, obstac}) =>{
-    const casilla = parseInt(obstac)
-    
-    //obtenemos la casilla donde está posicionado el ajente
+    //EXTRACT THE DATA FROM THE INPUT, CONTAINS AN ID
+    const casilla = parseInt(obstac)        
+    //WE USE THE FINDINDEX FUNCTION TO FIND THE DESIRED OBJECT
     const elementsCoord = tablero.findIndex(element => element.id === casilla)
-    //creamos copia del tablero
+    //WE MAKE A COPY OF THE ENVIROMENT
     let ajuste = [...tablero]
-    //actualizamos tablero con valor visitado true a la casilla donde se encontró el agente
+    //UPDATE THE ELEMENT FINDED
     ajuste[elementsCoord] = {...ajuste[elementsCoord], obs: true}
     return(ajuste)
 }
--- src/vista/Pantalla.js
+++ src/vista/Pantalla.js
@@ -3,24 +3,24 @@ import Casillas from './Casillas'
 import Sensores from './Sensores'
 
 import './Styles.css'
-import { evaluarS1, evaluarS2, evaluarS3,evaluarS4,evaluarS5,evaluarS6,evaluarS7,evaluarS8} from '../logica/exploracion'
-import { colorearagente, colorearcamino, colorearmeta, colorearobstaculo, crearentorno, crearmuros } from '../logica/calculosentorno'
+import { evaluarS1, evaluarS2, evaluarS3, evaluarS4, evaluarS5, evaluarS6, evaluarS7, evaluarS8 } from '../logica/exploracion'
+import { colorearagente, colorearmeta, colorearobstaculo, crearentorno } from '../logica/calculosentorno'
 
 export default class Pantalla extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            //sensores
+            //sensors
             s1: '', s2: '', s3: '', s4: '', s5: '', s6: '', s7: '', s8: '',
 
-            tablero: [],//configura el tablero o entorno
-            agente_p: { x: 1, y: 1 },//posición de nuestro agente, por defecto en 1,1
-            obstac: '',//dónde poner el obstáculo #de casilla 
-            meta: '',//dónde poner la meta #de casilla     
+            tablero: [],  //Contains the enviroment
+            agente_p: { x: 1, y: 1 },//Position of the agent, by default at 1,1
+            obstac: '',  //dónde poner el obstáculo #de casilla 
+            meta: '',    //State tas contains a cell ID, used to place "meta"
 
-            pathlist: [],
-            stop: false,
-            index: -2
+            pathlist: [],//Contains the last places visited
+            stop: false, //Defines when the agent has to stop
+            index: -2    //Needed to move through pathlist
         }
         this.sleep = this.sleep.bind(this)
 
@@ -51,24 +51,25 @@ export default class Pantalla extends Component {
         let loopstop = stop        //INITIAL STATE
 
         while (loopstop === false) {
-            //CAMBIA LA POSICIÓN DEL AGENTE
 
+            //SENSORS GET INFORMATION AROUND THE AGENT
             this.explorarentorno()
 
+            //After reading the enviroment, make decision
             await this.sleep(100)
             this.rules()
 
-            ////////////////////////////////////////////////////////////////////////
-            //UPDATES THE VIEW, VISUAL STUFF
 
+            //UPDATES THE VIEW, VISUAL STUFF
             await this.sleep(110)
             this.viewupdate()
 
+            //VERIFY If the stop state has changed
             const { stop } = this.state
             loopstop = stop //UPDATES THE STOP STATE
         }
 
-        //}
+
     }
 
     render() {
@@ -79,47 +80,44 @@ export default class Pantalla extends Component {
             s1, s2, s3, s4, s5, s6, s7, s8
         } = this.state
 
-
-
-
         return (
             <>
                 <button
                     onClick={this.llenartablero}
                     style={{ position: 'fixed', top: '10%', right: '2%' }}
                 >llenar tablero</button>
-                <input type='number' style={{ position: 'fixed', top: '17%', right: '2%' }} value={objetivo} onChange={this.objetivocambio} placeholder='insertar obstáculo # de casilla' />
+
+                <input type='number'
+                    style={{
+                        position: 'fixed',
+                        top: '17%', right: '2%'
+                    }}
+                    value={objetivo}
+                    onChange={this.objetivocambio}
+                    placeholder='insertar obstáculo # de casilla'
+                />
+
                 <button
                     onClick={this.definirobs}
                     style={{ position: 'fixed', top: '21%', right: '2%' }}
                 >colocar obs</button>
-                <input type='number' style={{ position: 'fixed', top: '27%', right: '2%' }} value={meta} onChange={this.metacambio} placeholder='definir meta # casilla' />
+
+                <input type='number'
+                    style={{
+                        position: 'fixed',
+                        top: '27%',
+                        right: '2%'
+                    }}
+                    value={meta}
+                    onChange={this.metacambio}
+                    placeholder='definir meta # casilla'
+                />
+
                 <button
                     onClick={this.definirmeta}
                     style={{ position: 'fixed', top: '31%', right: '2%' }}
-                >colocar meta</button>
-                {/*<button 
-                    onClick={this.crearmuralla}
-                    style={{position:'fixed',top:'41%',right:'2%'}}
-                >crear muralla</button>
-
-                <button 
-                    onClick={this.caminarnorte}
-                    style={{position:'fixed',top:'48%',right:'6%'}}
-                >ir norte</button>                
-                <button 
-                    onClick={this.caminarsur}
-                    style={{position:'fixed',top:'61%',right:'6%'}}
-                >ir sur</button>           
-                <button 
-                    onClick={this.caminareste}
-                    style={{position:'fixed',top:'54%',right:'2%'}}
-                >ir este</button>           
-                <button 
-                    onClick={this.caminaroeste}
-                    style={{position:'fixed',top:'54%',right:'10%'}}
-                >ir oeste</button>     */}
-
+                >colocar meta</button>            
+                
                 <button
                     onClick={this.start}
                     style={{ position: 'fixed', top: '70%', right: '2%' }}
@@ -128,6 +126,7 @@ export default class Pantalla extends Component {
 
                 {
                     tablero.length > 0 &&
+                    //WE ADD THE VISUAL ELEMENTS WHEN THE ENVIROMENT HAS BEEN CREATED
                     <>
                         <div
                             style={{
@@ -135,10 +134,12 @@ export default class Pantalla extends Component {
                                 display: 'grid',
                                 gridTemplateColumns: 'repeat(20, 1fr)',
                                 gridAutoRows: '20',
-                            }}
-                        >
+                            }}>
+                            
                             <Casillas tablero={tablero} />
+                        
                         </div>
+
                         <Sensores
                             s1={s1}
                             s2={s2}
@@ -149,7 +150,9 @@ export default class Pantalla extends Component {
                             s7={s7}
                             s8={s8}
                         />
-                    </>}
+                    </>
+                    
+                }
 
             </>
         )
@@ -160,8 +163,6 @@ export default class Pantalla extends Component {
         const { agente_p } = this.state
         const arr = crearentorno({ agente_p })
         this.setState({ tablero: arr, stop: false, agente_p: { x: 1, y: 1 }, index: -2 });
-        //console.log('Antes de pintar agente');
-        //this.paintAgente(agente_p,arr)
     }
 
     definirobs() {
@@ -200,8 +201,7 @@ export default class Pantalla extends Component {
             s7: estadoS7,
             s8: estadoS8
         })
-        //const entorno = {s1:estadoS1, s2:estadoS2, s3:estadoS3, s4:estadoS4, s5:estadoS5, s6:estadoS6, s7:estadoS7, s8:estadoS8}
-        //return entorno
+
     }
 
     objetivocambio(e) {
@@ -268,27 +268,32 @@ export default class Pantalla extends Component {
         let backmove = []
         let backlist = []
         let movimiento = []
+
+        //WHEN THE AGENT GETS TRAPED AROUND OBSTACLES, OR HAS ONLY VISITED PLACES AROUND
         if ((s1 === 'visi' || s1 === 'obst') && (s2 === 'visi' || s2 === 'obst') && (s3 === 'visi' || s3 === 'obst') && (s4 === 'visi' || s4 === 'obst') && (s5 === 'visi' || s5 === 'obst') && (s6 === 'visi' || s6 === 'obst') && (s7 === 'visi' || s7 === 'obst') && (s8 === 'visi' || s8 === 'obst')) {
             const { agente_p, pathlist, index } = this.state
             let minus = index
-            movimiento = agente_p //COPY OF THE AGENT POSITION {x:n, y:n}
-            //console.log('Movimiento ',movimiento)
+            movimiento = agente_p //COPY OF THE AGENT POSITION {x:n, y:n}            
             backlist = pathlist //LISTA ENTERA DE PASOS ANTERIORES
             backmove = pathlist.at(minus)  //X ITEM DE LA LISTA, anteriormente visitado
-            //console.log('Backmove ',backmove) 
-            //cosole.log(pathlist)
+
+            //We set the movement same as the selected object from pathlist
             movimiento.x = backmove.x
             movimiento.y = backmove.y
+
             backlist.push(agente_p)
-            //console.log(pathlist)
+            //WE DECREMENT THE VALUE -2 BECAUSE OF OFFSET REASONS
+            /*No decrementar el index resultaría en el agente moviéndose a
+              a la casilla en la última posición del pathlist cada vez que 
+              esta regla sea verdadera*/
             minus = minus - 2
-            //console.log('minus -2 =',minus)
-            //console.log(minus)
+
+
             this.setState({ agente_p: movimiento, index: minus, pathlist: backlist })
         }
 
 
-        //IF "META" IS NEARBY, MOVES TO THE PLACE IT IS///////////////////////////////////////////////////
+        /////////////////IF "META" IS NEARBY, MOVES TO THE PLACE IT IS///////////////////////////////////////////////////
         //IF IT IS AT NORTH
         if (s2 === 'meta') {
             this.caminarnorte()
@@ -314,8 +319,7 @@ export default class Pantalla extends Component {
             return
         }
 
-
-        //META NEARBY, AT CORNERS
+        ////////////////////////META NEARBY, AT CORNERS//////////////////////////////////////////////////////////////
         //IF NW AND N IS CLEAR
         if (s1 === 'meta' && s2 === 'vaci') {
             this.caminarnorte()
@@ -358,6 +362,8 @@ export default class Pantalla extends Component {
         }
 
 
+
+        //////////////////////////////DEFAULT MOVEMENTS, 4 WAYS, MOVE IF CLEAR ("vaci")//////////////////////////////////
         if (s2 === 'vaci') {
             this.caminarnorte()
             return
@@ -425,14 +431,13 @@ export default class Pantalla extends Component {
     }
 
     viewupdate() {
-        const { agente_p, tablero } = this.state//collects position of the agent            
+        //WE EXTRACT THE ACTUAL POSITION OF THE AGENT, AND THE ENVIROMENT
+        const { agente_p, tablero } = this.state
         //RECALCULAR CASILLAS VISITADAS Y COLOREAR POS ACTUAL DEL AGENTE
         const recalcular = colorearagente({ agente_p, tablero }) //Updates the enviroment string
-        //ACTUALIZAR ENTORNO
+        //UPDATE ENVIROMENT
         this.setState({ tablero: recalcular }) //Sets the enviroment string into the new state
 
-
-        //console.log(pathlist)//PRINTS LAST VISITED                        
     }
 
 }
